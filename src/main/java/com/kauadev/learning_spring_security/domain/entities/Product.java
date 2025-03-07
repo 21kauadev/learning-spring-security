84c5121a8b2bc64ac01881f961b23ff9861681e9
21kauadev <kauantnetsoares@gmail.com>
2025-03-07 10:33:08 -0300

    feat: add entity User

---
+++ src/main/java/com/kauadev/learning_spring_security/domain/entities/Product.java
@@ -0,0 +1,5 @@
+package com.kauadev.learning_spring_security.domain.entities;
+
+public class Product {
+
+}
---
+++ src/main/java/com/kauadev/learning_spring_security/domain/entities/User.java
@@ -0,0 +1,28 @@
+package com.kauadev.learning_spring_security.domain.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    private String email;
+    private String password;
+}
