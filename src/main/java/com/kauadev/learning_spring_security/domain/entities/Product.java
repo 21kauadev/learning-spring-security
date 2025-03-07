72d79fc397e50cfb2710eea3aae5ea366dca38d0
21kauadev <kauantnetsoares@gmail.com>
2025-03-07 10:36:47 -0300

    feat: add Product entity

--- src/main/java/com/kauadev/learning_spring_security/domain/entities/Product.java
+++ src/main/java/com/kauadev/learning_spring_security/domain/entities/Product.java
@@ -1,5 +1,29 @@
 package com.kauadev.learning_spring_security.domain.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantity;
 }
